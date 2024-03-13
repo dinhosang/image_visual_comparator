eae83873928b1660551f683ca333ef5bb2262b55
Douglas Sangster <33224828+dinhosang@users.noreply.github.com>
2024-03-13 00:37:37 +0000

    feat: refactoring run() to better handle tokio errors and to prepare for future image creation

--- src/errors/external/mod.rs
+++ src/errors/external/mod.rs
@@ -1,5 +1,7 @@
-use image::ImageError;
 use thiserror::Error;
+
+use image::ImageError;
+use tokio::task::JoinError;
 
 #[derive(Error, Debug)]
 #[error("Issue parsing file at location: '{location}'. Message: '{source_message}'")]
@@ -12,6 +14,22 @@ impl IOReadError {
     pub fn new(location: String, source: ImageError) -> Self {
         IOReadError {
             location,
+            source_message: source.to_string(),
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+#[error("Issue with tokio join set when performing '{action}'")]
+pub struct TokioJoinError {
+    action: String,
+    source_message: String,
+}
+
+impl TokioJoinError {
+    pub fn new(action: String, source: JoinError) -> Self {
+        TokioJoinError {
+            action,
             source_message: source.to_string(),
         }
     }
